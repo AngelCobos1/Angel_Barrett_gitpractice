5f7f585e9ab3ab5ff00a4989014f334a347881c7
Bnibling <bnibling@gmail.com>
2020-09-09 10:50:12 -0500

    Added test.py file to repo

---
+++ test.py
@@ -0,0 +1,23 @@
+# # dsi7_student_pair_work
+#
+# ## Purpose
+#
+# Public repo to turn in pair exercises during our Data Science Immersive course.
+#
+# ## Instructions
+#
+# Push your pair exercise notebooks (or other filetypes when appropriate) into the current week folder, using a file name with your pair's names in it. Make sure to use your names so you don't overwrite someone else's file.
+#
+# ## Example
+#
+# ```
+# cd dsi7_student_pair_work
+# mv python_practice_eda_alessandro_eniola_elizabeth.ipynb week01_python_statistics
+# git add week01_python_statistics/
+# git commit -m 'EDA assignment'
+# git pull
+# git push
+# ```
+
+#Super important code
+print('Hello world')
